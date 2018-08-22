75fcb562c43d8c4ddf2070412a572fb2a090ccdd
Suma Subbu <s3696186@student.rmit.edu.au>
2018-08-22 15:07:15 +1000

    Created Player class and TestPlayer Junit class

--- SimpleStrategicGame/src/simpleGame/Player.java
+++ SimpleStrategicGame/src/simpleGame/Player.java
@@ -1,9 +1,43 @@
 package simpleGame;
 
 /**
- * @author RudhraRaji
- * 21 Aug. 2018
+ * @author Suma Subbu
+ * 22 Aug. 2018
  */
-public class Player {
 
+public class Player {
+    private int x;
+    private int y;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+//    public isInitialPosition(int x, int y){
+//
+//    }
+    public void moveLeft(int count){ //method to move the player to the left of his position
+        this.x-=count;
+    }
+
+    public boolean isBlock(int x, int y){ //checking if the path is blocked by a wall
+        int blockx=x;
+        int blocky=y;
+        if(this.x==blockx+1 || this.x==blocky-1)
+            return true;
+        else if(this.y==blocky+1 || this.y==blocky-1)
+            return true;
+        else return false;
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
+
---
+++ SimpleStrategicGame/src/testGame/TestPlayer.java
@@ -0,0 +1,31 @@
+
+package testGame;
+/**
+ * @author Suma Subbu
+ * 22 Aug. 2018
+ */
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+import simpleGame.*;
+
+import org.junit.Test;
+
+public class TestPlayer {
+    Player player;
+    @Before
+    public void setUp() {
+        player = new Player(5,5);
+    }
+    @Test
+    public void moveLeftTest() {
+        player.moveLeft(2);
+        assertEquals(3,player.getX());
+    }
+    @Test
+    public void isBlockTest() {
+        assertEquals(false, player.isBlock(3,2));
+    }
+}
+
