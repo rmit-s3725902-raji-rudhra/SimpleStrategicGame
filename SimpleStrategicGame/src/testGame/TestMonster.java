2bca1fbc535c77a19957597ddc9f06aebbfe30a2
rmit-s3701080-sakshi-sharma <42196629+rmit-s3701080-sakshi-sharma@users.noreply.github.com>
2018-08-23 00:42:48 +0000

    Create TestMonster.java

---
+++ SimpleStrategicGame/src/testGame/TestMonster.java
@@ -0,0 +1,90 @@
+package testGame;
+
+
+import static org.junit.Assert.*;
+
+
+import org.junit.After;
+
+
+import org.junit.Before;
+
+
+import org.junit.Test;
+
+
+import simpleGame.Monster;
+
+
+
+public class TestMonster {
+
+
+	
+
+
+
+Monster M;
+
+
+
+	@Before
+
+
+	public void setUP()
+
+
+	{
+
+
+		M = new Monster(1,1);
+
+
+		
+
+
+		//x=1;
+
+
+		//y=1;
+
+
+	}
+
+
+
+	@Test
+
+
+	public void Move() {
+
+
+		M.Move();
+
+
+		assertEquals(2,M.getX());
+
+
+	}
+
+
+	@Test
+
+
+	public void notMove() {
+
+
+	//	M.notMove();
+
+
+		//boolean Nmove = M.notMove("")
+
+
+		assertNotEquals(false,M.notMove());
+
+
+	}
+
+
+
+}
