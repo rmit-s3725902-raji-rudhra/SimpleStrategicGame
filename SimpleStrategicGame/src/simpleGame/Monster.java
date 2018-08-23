d62c4b0f2d73471af33eadb61c4951a83a938364
rmit-s3701080-sakshi-sharma <42196629+rmit-s3701080-sakshi-sharma@users.noreply.github.com>
2018-08-23 00:41:23 +0000

    Update Monster.java

--- SimpleStrategicGame/src/simpleGame/Monster.java
+++ SimpleStrategicGame/src/simpleGame/Monster.java
@@ -1,10 +1,108 @@
 package simpleGame;
 
+
+
 /**
+
+
 * @author RudhraRaji
 
+
+
 * 21 Aug. 2018
+
+
 */
+
+
 public class Monster {
+
+
+	private int Mspeed;
+
+
+	private int x;
+
+
+
+	
+
+
+
+	public Monster(int initialX,int initialMspeed)
+
+
+	{
+
+
+		x=initialX;
+
+
+		Mspeed=initialMspeed;
+
+
+	
+
+
+	}
+
+
+
+	public void Move()
+
+
+	{
+
+
+		if (x+Mspeed <=2);
+
+
+		x=x+Mspeed;
+
+
+	}
+
+
+	public boolean notMove()
+
+
+	{
+
+
+		if (Mspeed== 1)
+
+
+			return true;
+
+
+		else
+
+
+			return false;
+
+
+	}
+
+
+	
+
+
+	
+
+
+	public int getX()
+
+
+	{
+
+
+		return x;
+
+
+	}
+
+
+	
+
 
 }
